605c27380d990f27eb3f65fd07e36828db5b47df
feliperod0519 <feliperod0519@hotmail.com>
2019-11-01 15:44:56 -0400

    hello felipeRod

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormControl, FormGroup, FormBuilder } from '@angular/forms';
+import { PersonData, ContactRequest } from '../../models/peopleRequest'
 
 @Component({
   selector: 'app-registration',
@@ -11,20 +12,56 @@ export class RegistrationComponent implements OnInit {
   EMAIL_REGEX = "[a-z0-9!#$%&'*+\/=?^_`{|}~.-]+@[a-z0-9]([a-z0-9-]*[a-z0-9])?(\.[a-z0-9]([a-z0-9-]*[a-z0-9])?)*";
   registrationForm: FormGroup;
 
-  constructor() { }
+  countries = ["Indonesia","Comore Islands","Canada"];
+  requestType = ["None", "Unknown", "Claim"];
+
+  selectedCountry:string = "";
+
+  constructor(private formBuilder: FormBuilder) 
+  { 
+      this.registrationForm = this.createFormGroup_1(formBuilder);
+  }
 
   ngOnInit() {
-    console.log('hello');
-    this.registrationForm = new FormGroup({ 
-                                            firstName: new FormControl('FelipeRod',Validators.required),
-                                            lastName: new FormControl(''),
-                                            email: new FormControl('', [Validators.required, Validators.pattern(this.EMAIL_REGEX)])
-                                          });
+    console.log('hello'); 
   }
 
-  onSubmit(formValue){
-    console.log(formValue);
-    console.log(this.registrationForm.value);
+  createFormGroup_1(formBuilder:FormBuilder){
+      return formBuilder.group({
+                                  personalData: formBuilder.group(new PersonData()),
+                                  requestType:'',
+                                  text:''
+                              });
   }
 
+  /*
+  const result: ContactRequest = Object.assign({}, this.contactForm.value);
+    result.personalData = Object.assign({}, result.personalData);
+  */
+
+  onSubmit(){
+    console.log("onsubmit");
+    const result: ContactRequest = Object.assign({},this.registrationForm.value);  
+    result.personalData = Object.assign({},result.personalData);
+    console.log(result);
+  }
+
+  revert(){
+    console.log("revert");
+    this.registrationForm.reset();
+    this.registrationForm.reset({
+                                  personalData: new PersonData(),
+                                  requestType:'',
+                                  text:''
+                                });
+  }
+
+  get selectedCountryMod(){
+    return this.selectedCountry;
+  }
+
+  set selectedCountryMod(value){
+    console.log(value);
+    this.selectedCountry = value;
+  }
 }
---
+++ src/models/peopleRequest.ts
@@ -0,0 +1,15 @@
+export class ContactRequest{
+   personalData: PersonData;
+   requestType: any = '';
+   text: string = '';
+}
+export class PersonData{
+    id: string= '';
+    last_name: string= '';
+    first_name: string= '';
+    email: string= '';
+    photo: string = 'default.jpg';
+    mobile: string = '555-888-9999';
+    country: string = "Canada";
+
+}
